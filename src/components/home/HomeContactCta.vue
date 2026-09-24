09af7b612a8ee02dc5b98d06a3765bc7ca1ad39e
andreaperezgon <andreaperezgon@gmail.com>
2026-09-24 11:29:16 +0200

    feat: add home contact CTA

---
+++ src/components/home/HomeContactCta.vue
@@ -0,0 +1,3 @@
+git add .
+git commit -m "feat: add home gallery section"
+git push
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,6 +3,7 @@ import HomeHero from '../components/home/HomeHero.vue'
 import HomeAbout from '../components/home/HomeAbout.vue'
 import HomeServices from '../components/home/HomeServices.vue'
 import HomeGallery from '../components/home/HomeGallery.vue'
+import HomeContactCta from '../components/home/HomeContactCta.vue'
 </script>
 
 <template>
@@ -11,5 +12,6 @@ import HomeGallery from '../components/home/HomeGallery.vue'
     <HomeAbout />
     <HomeServices />
     <HomeGallery />
+    <HomeContactCta />
   </main>
 </template>
