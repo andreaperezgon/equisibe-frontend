de47e9f7e1e9f23f0fbe18545a5359474d576c12
andreaperezgon <andreaperezgon@gmail.com>
2026-09-24 09:08:28 +0200

    feat: configure router and add home hero

---
+++ src/views/HomeView.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <section>
+      <p>Taller de confección</p>
+
+      <h1>
+        Ropa hecha desde cero, a la medida de quien la lleva
+      </h1>
+
+      <p>
+        En Equisibé cortamos, cosemos y arreglamos. Trajes, vestidos y
+        prendas de diario con patrón propio y acabados a mano.
+      </p>
+
+      <div>
+        <RouterLink to="/shop">
+          Ver la tienda
+        </RouterLink>
+
+        <RouterLink to="/contact">
+          Pedir cita
+        </RouterLink>
+      </div>
+    </section>
+  </main>
+</template>
